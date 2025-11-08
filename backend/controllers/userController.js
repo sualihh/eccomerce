da4a2b249b5e1605b4a6264539e457f10ef10452
sualih <sualihmohammed53@gmail.com>
2025-11-08 16:58:27 +0300

    commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+
+
+// route for login
+const loginUser = async (req, res) => {
+    
+}
+
+
+
+// route for registration
+
+const registerUser = async (req, res) => {
+
+}
+
+
+// for admin
+const adminLogin= async (req, res) => {
+
+}
+
+export {loginuser, registerUser, adminLogin}
--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,23 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDb from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 
 // app config
 
 const app = express()
 const port = process.env.PORT || 4000;
 connectDb();
-connectCloudinary
+connectCloudinary();
+
 
 // midlewares
 app.use(express.json());
 app.use(cors())
 
 // api end points
+
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
    res.send("API WORKS Successfully") 
