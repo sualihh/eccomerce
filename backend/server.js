1524a4c5cc42c8f8a17d7583032b30f79113131d
sualih <sualihmohammed53@gmail.com>
2025-11-08 16:00:19 +0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors'
 import 'dotenv/config'
 import connectDb from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 // app config
 
 const app = express()
 const port = process.env.PORT || 4000;
 connectDb();
+connectCloudinary
 
 // midlewares
 app.use(express.json());
