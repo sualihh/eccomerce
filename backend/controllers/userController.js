4b3a099786330a9c0bb321d0c13355c8f6f66d59
sualih <sualihmohammed53@gmail.com>
2025-11-08 20:25:30 +0300

    commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,6 @@
+import validator from "validator"
+import bcrypt from "bcrypt"
+import userModel from "../models/userModel";
 
 
 // route for login
@@ -10,7 +13,45 @@ const loginUser = async (req, res) => {
 // route for registration
 
 const registerUser = async (req, res) => {
-    res.json({msg: "register api working"})
+    //res.json({msg: "register api working"})
+    try {
+        
+        const {name, email, password} = req.body;
+
+        // check if the user is exist
+
+        const exist = await userModel.findOne({email});
+
+        if(exist){
+            return res.json({success:false, msg: 'user alredy exist'})
+        }
+
+        // validate email and password 
+
+        if (!validator.isEmail(email)) {
+
+            return res.json({success:false, msg: 'enter valid email'})
+            
+        }
+        if (password.length < 8) {
+
+            return res.json({success:false, msg: 'enter 8 character password'})   
+        }
+
+        // hash password
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name, 
+            email, 
+            password:hashedPassword
+        })
+
+    } catch (error) {
+        
+    }
 }
 
 
