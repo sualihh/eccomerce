6df4f6f10636be3e3b8fbac4f914350adc456a8a
sualih <sualihmohammed53@gmail.com>
2025-11-09 20:21:23 +0300

    commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,23 +1,57 @@
-import validator from "validator"
-import productModel from "../models/productModel.js";
+import { v2 as cloudinary} from 'cloudinary'
+import productModel from '../models/productModel.js'
 
 // to add
-const addProduct = (req, res) => {
+const addProduct = async (req, res) => {
     try {
         const {name, description, price, category, subCategory, sizes, bestSeller} = req.body;
 
-        const image1 = req.files.image1[0];
-        const image2 = req.files.image2[0];
-        const image3 = req.files.image3[0];
-        const image4 = req.files.image4[0];
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
+ 
+  // to ignore empty images
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
+
+
+      // toupload in to cloudinary      
+        let imagesURL = await Promise.all(
+          images.map(async (item)=> {
+            // to change buffer in to base 64
+            const base64 = `data:${item.mimetype};base64,${item.buffer.toString("base64")}`;
+
+            let result = await cloudinary.uploader.upload(base64, {resource_type:'image'});
+            return result.secure_url
+          } )
+        )
 
         console.log(name, description, price, category, subCategory, sizes, bestSeller);
-        console.log(image1, image2, image3, image4);
-        res.json({})
+        
+
+        // to store in mongodb
+        const productData = {
+          name, 
+          description,
+          price: Number(price),
+          category,
+          subCategory,
+          bestSeller: bestSeller === 'true' ? true : false,
+          sizes:   JSON.parse(sizes) ,
+          images: imagesURL,
+          date:Date.now()
+        }
+
+
+        console.log(productData);
+
+        const product = new productModel(productData);
+        await product.save();
+
+        res.json({success:true, msg: "Product added successfully"})
 
     } catch (error) {
         console.log(error);
-        
         res.json({success:false, msg: error.msg})
     }
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,11 +5,7 @@ import upload from "../middleware/multer.js";
 
 const productRouter = express.Router();
 
-productRouter.post('/add',upload.fields([                
-    {name:"image1", maxCount:1},
-    {name:"image2", maxCount:1},
-    {name:"image3", maxCount:1},
-    {name:"image4", maxCount:1}]),
+productRouter.post('/add',upload.fields([ {name:"image1", maxCount:1},{name:"image2", maxCount:1},{name:"image3", maxCount:1},{name:"image4", maxCount:1}]),
      addProduct)
 productRouter.post('/list', listProduct)
 productRouter.post('/remove', removeProduct)
