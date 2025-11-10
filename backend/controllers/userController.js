39b428e5970e5328de4a03da30e3277c4feac3fa
sualih <sualihmohammed53@gmail.com>
2025-11-10 20:47:53 +0300

    updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -68,6 +68,7 @@ const registerUser = async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
+        
         const newUser = new userModel({
             name, 
             email, 
