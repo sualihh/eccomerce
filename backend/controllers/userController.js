234815bddd1bc65a44aae5689ba46ac6df74c07f
sualih <sualihmohammed53@gmail.com>
2025-11-10 20:49:17 +0300

    updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -96,7 +96,6 @@ const adminLogin= async (req, res) => {
         const {email, password} = req.body;
 
         
-        
         if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
             const token = jwt.sign(email+password,process.env.JWT_SECRET);
             res.json({success:true, token})
