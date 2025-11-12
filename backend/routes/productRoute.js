be213b522ee9aa9b8d2beecb4adb9bf8874aca63
sualih <sualihmohammed53@gmail.com>
2025-11-12 11:06:42 +0300

    updated

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,7 +6,14 @@ import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
-productRouter.post('/add',adminAuth,upload.fields([ {name:"image1", maxCount:1},{name:"image2", maxCount:1},{name:"image3", maxCount:1},{name:"image4", maxCount:1}]),
+productRouter.post('/add',
+     adminAuth,
+     upload.fields([ 
+          {name:"image1", maxCount:1},
+          {name:"image2", maxCount:1},
+          {name:"image3", maxCount:1},
+          {name:"image4", maxCount:1}
+     ]),
      addProduct)
 productRouter.get('/list', listProduct)
 productRouter.post('/remove',adminAuth, removeProduct)
