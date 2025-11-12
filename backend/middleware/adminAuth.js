ab1fc1173fd2cb80fa17ed9cf07c7e20d2084657
sualih <sualihmohammed53@gmail.com>
2025-11-12 09:18:55 +0300

    commit

---
+++ backend/middleware/adminAuth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+
+const adminAuth = async (req, res, next) => { 
+    try {
+        // to get tokens from header
+        const {token} = req.headers;
+
+        // check token if available
+
+        if (!token) {
+            return res.json({
+                success:false,
+                message:"NOT AUTHORIZED"
+            })
+        }
+        // decode token
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
+
+        // CHECKING THE decoded token is equal to admin email and password
+
+        if (token_decode !== process.env.ADMIN_EMAIL+process.env.ADMIN_PASSWORD)
+    } catch (error) {
+        
+    }
+}
