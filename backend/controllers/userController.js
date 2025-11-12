7173231736668fa5c038220f5d61b352a2ca7ea4
sualih <sualihmohammed53@gmail.com>
2025-11-12 11:14:08 +0300

    updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,9 +26,15 @@ const loginUser = async (req, res) => {
 
         if (isMatch) {
             const token = createToken(user._id);
-            res.json({success:true, token});
+            res.json({
+                success:true, 
+                token
+            });
         } else {
-            res.json({success:false, msg: 'invalid credentials'})
+            res.json({
+                success:false, 
+                msg: 'invalid credentials'
+            })
         }
     } catch (error) {
         console.log(error);
