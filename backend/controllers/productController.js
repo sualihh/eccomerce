d64636b9aa9a65226d1c3cf3f2ccd1c2acb965eb
sualih <sualihmohammed53@gmail.com>
2025-11-12 10:41:10 +0300

    updated

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,10 +6,10 @@ const addProduct = async (req, res) => {
     try {
         const {name, description, price, category, subCategory, sizes, bestSeller} = req.body;
 
-        const image1 = req.files.image1 && req.files.image1[0];
-        const image2 = req.files.image2 && req.files.image2[0];
-        const image3 = req.files.image3 && req.files.image3[0];
-        const image4 = req.files.image4 && req.files.image4[0];
+        const image1 = req.files.image1?.[0];
+        const image2 = req.files.image2?.[0];
+        const image3 = req.files.image3?.[0];
+        const image4 = req.files.image4?.[0];
  
   // to ignore empty images
         const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
