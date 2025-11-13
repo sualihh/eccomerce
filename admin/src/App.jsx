54415ecc72919baecef0d18126bee3c4e2bce9f5
sualih <sualihmohammed53@gmail.com>
2025-11-13 11:19:25 +0300

    updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,38 +1,38 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import { Routes, Route } from 'react-router-dom'
-import Add from './pages/Add'
-import List from './pages/List'
-import Orders from './pages/Orders'
-import Login from './pages/Login'
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./pages/Login";
 
 const App = () => {
   // state variable  for admin if it authenticated or not
-  const {token, setToken} = useState("");
-
+  const { token, setToken } = useState("");
 
   return (
     <div className="bg-gray-50 min-h-screen">
-      {token === ""
-       ? <Login /> : 
-      <>
-      <Navbar />
-      <hr />
-      <div className='flex w-full'>
-        <Sidebar />
-        <div className='w-[70] mx-auto ml=[max(5vw,25px)] my-8 text-gray-600 text-base'>
-          <Routes>
-            <Route path='/add' element={<Add />} />
-            <Route path='/list' element={<List />} />
-            <Route path='/order' element={<Orders />} />
-          </Routes>
-        </div>
-      </div>
-      </>
-}
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70] mx-auto ml=[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/order" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
