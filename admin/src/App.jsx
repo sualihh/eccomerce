98b4566977be8eb8f7eaa33f594d933f5612c7d5
sualih <sualihmohammed53@gmail.com>
2025-11-13 10:53:00 +0300

    updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
-    <div className="background-black">
+    <div className="bg-gray-50 min-h-screen">
       <Navbar />
+      <hr />
+      <div className='flex w-full'>
+        <Sidebar />
+      </div>
     </div>
   )
 }
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import {assets} from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
-      <img className='w-[max(10%, 80px)]' src={assets.logo} alt="logo" />
-      <button className='text-9xl'>Logout</button>
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+      <img className='w-[max(10%,80px)]' src={assets.logo} alt="logo" />
+      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
