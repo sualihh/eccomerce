05ff420a2bc578b177dbb0ec3ddc4329ceaf7917
sualih <sualihmohammed53@gmail.com>
2025-11-13 10:02:36 +0300

    updated

---
+++ admin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className="background-black">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div>
+      <img className='w-[max(10%, 80px)]' src={assets.logo} alt="logo" />
+      <button>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
