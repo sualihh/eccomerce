20f3e0747af90c038915c76fa62f6276d830aba3
sualih <sualihmohammed53@gmail.com>
2025-11-16 19:53:17 +0300

    updated

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,7 +1,7 @@
 import express from "express"
-import { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus } from "../controllers/orderController"
-import adminAuth from "../middleware/adminAuth"
-import authUser from "../middleware/auth"
+import { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus } from "../controllers/orderController.js"
+import adminAuth from "../middleware/adminAuth.js"
+import authUser from "../middleware/auth.js"
 
 const orderRouter = express.Router()
 
