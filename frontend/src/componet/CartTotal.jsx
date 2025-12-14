cd225b98c2cbce9ba98f905a10349e90cb192767
sualih <sualihmohammed53@gmail.com>
2025-12-15 00:23:01 +0300

    updated

--- frontend/src/componet/CartTotal.jsx
+++ frontend/src/componet/CartTotal.jsx
@@ -18,7 +18,7 @@ const CartTotal = () => {
         </div>
         <div className='flex justify-between'>
           <p>Shipping Fee</p>
-          <p>{currency}{delivery_fee}.00</p>
+          <p>{currency}</p>
         </div>
 
 
