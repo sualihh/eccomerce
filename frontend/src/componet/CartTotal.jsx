aade0c389b1bc9ecf5260cdbad1e29a26b0e8b8f
sualih <sualihmohammed53@gmail.com>
2025-12-15 00:26:53 +0300

    updated

--- frontend/src/componet/CartTotal.jsx
+++ frontend/src/componet/CartTotal.jsx
@@ -14,6 +14,7 @@ const CartTotal = () => {
       <div className='flex flex-col gap-2 mt-2 text-sm'>
         <div className='flex justify-between'>
             <p>Subtotal</p>
+            <p>.00</p>
         </div>
         
 
