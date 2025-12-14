863f4288b5637c9e450a03566907b6d7f5479449
sualih <sualihmohammed53@gmail.com>
2025-12-15 00:28:22 +0300

    updated

--- frontend/src/componet/CartTotal.jsx
+++ frontend/src/componet/CartTotal.jsx
@@ -23,6 +23,7 @@ const CartTotal = () => {
 
 
         <div className='flex justify-between'>
+          <b>Total</b>
           <b>{currency}{getCartAmount() === 0 ? 0:getCartAmount() + delivery_fee}.00</b>
         </div>
       </div> 
