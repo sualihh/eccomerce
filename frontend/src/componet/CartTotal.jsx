4fd3a4d1799f6fbbb4cf62b8ebd0b332bfc6ed37
sualih <sualihmohammed53@gmail.com>
2025-12-15 00:25:49 +0300

    updated

--- frontend/src/componet/CartTotal.jsx
+++ frontend/src/componet/CartTotal.jsx
@@ -7,15 +7,7 @@ const CartTotal = () => {
   const {currency, delivery_fee, getCartAmount } = useContext(ShopContext)
   return (
     <div >
-      <  
-
-      <div >
-        
-        
-
-
-        
-      </div> 
+      
     </div>
   )
 }
