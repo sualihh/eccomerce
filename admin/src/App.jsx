e678ca1ea5995404a4864f583313ac88a507256e
sualih <sualihmohammed53@gmail.com>
2026-01-05 20:26:13 +0300

    complete by adding SRS

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -13,6 +13,8 @@ export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export const currency = '$'
 
 const App = () => {
+
+  
   // state variable  for admin if it authenticated or not
   const [ token, setToken ] = useState(localStorage.getItem("token")?localStorage.getItem("token"):"");
 
